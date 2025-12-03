f3685cf90d2dd5c5c446678ec736471dfdee921d
Eminhan Özil <56584914+eozil@users.noreply.github.com>
2025-12-03 09:47:41 +0100

    channel mapping added

--- mrid_utils/chmap.py
+++ mrid_utils/chmap.py
@@ -7,11 +7,10 @@ import numpy as np
 import math
 import plotly.graph_objects as go
 from scipy.optimize import minimize
+import channel_mapper
 
-
-# TODO: Henriette, you can incorporate this main function into your pipeline as you wish. I am only putting it here
-# to show you the flow
-def main(mrid, savepath, sessionpath, root, weighted_loss_f="density", bundle_start=0):
+# TODO: Henriette, you can incorporate this main function into your pipeline as you wish. I am only putting it here to show you the flow
+def main(mrid, savepath, sessionpath, root, weighted_loss_f="density", bundle_start=0, map_channels_boolean=True):
     """
     Localizes the electrode channels. Takes input;
     filename:
@@ -41,26 +40,24 @@ def main(mrid, savepath, sessionpath, root, weighted_loss_f="density", bundle_st
 
     fitted_points = register_bundle(gaussian_centers_3d, mrid_dict[mrid], bundle_start, weighted_loss_f=weighted_loss_f,
                                            visualization=True)
+    if map_channels_boolean:
+        # Mapping the channels to physical coordinate indeces (integers) in MRI space
+        ch_coords = channel_mapper.map_electrodes_main(fitted_points, mrid_dict[mrid])
+        np.save(os.path.join(sessionpath, "channel_mri_coordinates.npy"), ch_coords[0])
 
-    # TODO
-    # if chMap.any():
-    #     # Mapping the channels to physical coordinate indeces (integers) in MRI space
-    #     ch_coords = map_electrodes_main(fitted_points, mrid_dict[mrid])
-    #     np.save(os.path.join(mridpath, "channel_mri_coordinates.npy"), ch_coords[0])
-    #
-    #     moving_idx_filename = "moving_img_resampled25um-indeces.npy"
-    #     fixed_idx_filename = "fixed_img-indeces.npy"
-    #     moving_idx_path = os.path.join(sessionpath, "registration", moving_idx_filename)
-    #     fixed_idx_path = os.path.join(sessionpath, "registration", fixed_idx_filename)
-    #     print("Loading the moving coordinates: " + moving_idx_path)
-    #     print("Loading the fixed coordinates: " + fixed_idx_path)
-    #     moving_coordinates = np.load(moving_idx_path)
-    #     fixed_coordinates = np.load(fixed_idx_path)
-    #
-    #     # Mapping the channel coordinates to the Atlas space
-    #     dwi1Dsignal = map_channels_to_atlas(chMap, ch_coords, moving_coordinates, fixed_coordinates, savepath=mridpath)
-    #     np.save(os.path.join(mridpath, "dwi_1D_cross_section_pixel_values.npy"), dwi1Dsignal)
-    
+        moving_idx_filename = "moving_img_resampled25um-indeces.npy"
+        fixed_idx_filename = "fixed_img-indeces.npy"
+        moving_idx_path = os.path.join(sessionpath, "registration", moving_idx_filename)
+        fixed_idx_path = os.path.join(sessionpath, "registration", fixed_idx_filename)
+        print("Loading the moving coordinates: " + moving_idx_path)
+        print("Loading the fixed coordinates: " + fixed_idx_path)
+        moving_coordinates = np.load(moving_idx_path)
+        fixed_coordinates = np.load(fixed_idx_path)
+
+        # Mapping the channel coordinates to the Atlas space
+        dwi1Dsignal = channel_mapper.map_channels_to_atlas(ch_coords, moving_coordinates, fixed_coordinates, savepath=mridpath)
+        np.save(os.path.join(sessionpath, "dwi_1D_cross_section_pixel_values.npy"), dwi1Dsignal)
+
     return
 
 
